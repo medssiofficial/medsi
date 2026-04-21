447f0a7cf84ac76ccb9c61a44cbfbd186050389d
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2026-04-21 23:45:29 +0530

    feat: :sparkles: add login components

---
+++ apps/doctor/screens/login-screen/components/hero/login-hero.tsx
@@ -0,0 +1,19 @@
+const LoginHero = () => {
+	return (
+		<div className="h-screen bg-primary text-primary-foreground w-1/2 xl:flex hidden flex-col justify-center px-7 py-4 gap-4 font-sans">
+			<div className="w-full flex flex-col max-w-142 gap-8">
+				<h1 className="font-heading 2xl:text-3xl text-2xl font-bold">
+					Medssi
+				</h1>
+				<p className="text-muted-foreground 2xl:text-lg text-base">
+					Doctor Portal
+				</p>
+				<p className="text-muted-foreground font-semibold 2xl:text-4xl text-3xl">
+					Conneting medical expertise with patients worldwide
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default LoginHero;
--- apps/doctor/screens/login-screen/screen.tsx
+++ apps/doctor/screens/login-screen/screen.tsx
@@ -1,7 +1,9 @@
+import { LoginHero } from "./components/hero";
+
 const LoginScreen = () => {
 	return (
 		<main className="w-full min-h-screen flex bg-background text-foreground font-sans">
-			<></>
+			<LoginHero />
 		</main>
 	);
 };
