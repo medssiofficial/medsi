617c232e9de2cae273e5cdd956e90a805952ca2e
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2026-04-24 22:21:42 +0530

    feat: :sparkles: add clerk user creation webhook

--- apps/webhooks/config/server-env.ts
+++ apps/webhooks/config/server-env.ts
@@ -1,6 +1,11 @@
 import { createEnv } from "@t3-oss/env-nextjs";
+import z from "zod";
 
 export const SERVER_ENV = createEnv({
-	server: {},
-	runtimeEnv: {},
+	server: {
+		DOCTOR_CLERK_SIGNING_SECRET: z.string(),
+	},
+	runtimeEnv: {
+		DOCTOR_CLERK_SIGNING_SECRET: process.env.DOCTOR_CLERK_SIGNING_SECRET,
+	},
 });
