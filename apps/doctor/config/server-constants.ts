03f38f3d14958d0d2e0a8d11c3c35debacc052ec
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2026-04-24 22:59:39 +0530

    feat: :sparkles: add middleware constants

---
+++ apps/doctor/config/server-constants.ts
@@ -0,0 +1,5 @@
+export const SIGN_IN_URL = "/auth/login";
+export const SIGN_UP_URL = "/auth/sign-up";
+export const DASHBOARD_URL = "/dashboard";
+
+export const PUBLIC_ROUTES = [SIGN_IN_URL, SIGN_UP_URL];
--- apps/doctor/config/server-env.ts
+++ apps/doctor/config/server-env.ts
@@ -4,8 +4,12 @@ import z from "zod";
 export const SERVER_ENV = createEnv({
 	server: {
 		CLERK_SECRET_KEY: z.string(),
+		HOST: z.string(),
 	},
 	runtimeEnv: {
 		CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+		HOST: process.env.HOST,
 	},
 });
+
+export const serverEnv = SERVER_ENV;
