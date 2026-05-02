c37b5c46ff9c60a5d2431c1756d75bec28376bb7
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2026-05-03 01:37:09 +0530

    feat: :sparkles: add trigger to web

---
+++ apps/web/trigger.config.ts
@@ -0,0 +1,26 @@
+import { prismaExtension } from "@trigger.dev/build/extensions/prisma";
+import { defineConfig } from "@trigger.dev/sdk";
+
+export default defineConfig({
+	project: "proj_saxijmnvibfnofchjkov",
+	runtime: "bun",
+	dirs: ["./trigger"],
+	retries: {
+		enabledInDev: false,
+		default: {
+			maxAttempts: 3,
+			minTimeoutInMs: 1000,
+			maxTimeoutInMs: 10000,
+			factor: 2,
+			randomize: true,
+		},
+	},
+	maxDuration: 3600,
+	build: {
+		extensions: [
+			prismaExtension({
+				mode: "modern",
+			}),
+		],
+	},
+});
